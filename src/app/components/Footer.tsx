cce2901ca4596ece0b669b1d6e2fd88d39686e91
Lexani42 <33593776+Lexani42@users.noreply.github.com>
2025-07-18 01:25:42 +0300

    Add footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Footer() {
+    return (
+        <footer className="py-6 px-4 bg-gray-100 text-center text-sm text-gray-600">
+            <p>{new Date().getFullYear()} Oleksii Syrov. All rights reserved.</p>
+        </footer>
+    );
+}
