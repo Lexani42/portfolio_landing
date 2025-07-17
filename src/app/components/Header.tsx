57c5f517a72d34962394f45e2ceae5bbe69ff45e
Lexani42 <33593776+Lexani42@users.noreply.github.com>
2025-07-18 02:04:11 +0300

    Change header styling, add closing header while scrolling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,46 @@
 'use client';
 
+import { useEffect, useState } from "react";
+import classNames from 'classnames';
+
 export default function Header() {
+    const [isVisible, setIsVisible] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+    
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+            setIsVisible(lastScrollY > currentScrollY || currentScrollY < 10);
+            setLastScrollY(currentScrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll, { passive: true });
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [lastScrollY]);
+
     return (
-        <header className="fixed top-0 left-0 w-full z-50 bg-white bg-opacity-90 backdrop-blur shadow-sm">
-            <nav className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
-                <div className="text-xl font-bold text-gray-800">Syrov Oleksii</div>
-                <ul className="fkex gap-6 text-gray-700 font-medium text-sm">
-                    <li>
-                        <a href="#about" className="hover:text-blue-600 transition">About</a>
-                    </li>
-                    <li>
-                        <a href="#contact" className="hover:text-blue-600 transition">Contact</a>
-                    </li>
-                </ul>
-            </nav>
+        <header 
+            className={classNames(
+                'fixed top-0 left-0 w-full z-50 transition-transform duration-300',
+                {
+                    'transform -translate-y-full': !isVisible,
+                    'transform translate-y-0': isVisible,
+                }
+            )}
+        >
+            <div className="bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 backdrop-blur-md shadow-sm">
+                <nav className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center text-white ">
+                    <div className="text-xl font-extrabold tesxt-sky-400">Oleksii Syrov</div>
+                    <ul className="flex gap-6 font-semibold text-sm">
+                        <li>
+                            <a href="#about" className="px-4 oy-2 rounded-full border border-sky-500 text-sky-300 hover:bg-sky-500 hover:text-gray-900 transition-colors duration-300">About</a>
+                        </li>
+                        <li>
+                            <a href="#contact" className="px-4 oy-2 rounded-full border border-sky-500 text-sky-300 hover:bg-sky-500 hover:text-gray-900 transition-colors duration-300">Contacts</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
