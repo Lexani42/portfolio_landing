f82e02149c6ec148795f601541dc50853b8889ba
Lexani42 <33593776+Lexani42@users.noreply.github.com>
2025-07-18 20:35:28 +0300

    Add profile picture

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,10 +1,20 @@
 'use client';
 
+import Image from "next/image";
+
 export default function About() {
     return (
         <section id="about" className="min-h-screen py-20 px-6 text-gray-400">
             <div className="max-w-4xl mx-auto text-center">
                 <h2 className="text-3xl md:text-4xl font-bold mb-6 text-gray-200">About Me</h2>
+                <div className="relative md:w-64 md:h-64 rounded-full overflow-hidden border-4 border-gray-800 shadow-lg mx-auto">
+                    <Image 
+                        src="/images/profile.png"
+                        alt="Oleksii Syrov"
+                        fill
+                        className="object-cover"
+                    />
+                </div>
                 <p className="text-lg md:text-xl mb-6 text">
                     My name is Oleksii. I'm DevOps Engineer passionate about infrastructure automation, CI/CD, cloud-native and bare-metal technologies. I help teams shipping their products faster, more reliably and with confidence
                 </p>
