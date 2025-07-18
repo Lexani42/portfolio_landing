41baf48c3565f7feb707fd2ad2fd655c26c75d1c
Lexani42 <33593776+Lexani42@users.noreply.github.com>
2025-07-18 23:41:16 +0300

    Add aboutDefailt subsection and json reading in it

---
+++ src/app/components/About/AboutDetails.tsx
@@ -0,0 +1,33 @@
+import classNames from "classnames";
+import { useState } from "react";
+import details from '@/data/aboutDetails.json'
+
+export default function AboutDetails() {
+    const [showMore, setShowMore] = useState(false);
+    
+    return (
+        <div>
+            <button
+                onClick={() => setShowMore(!showMore)}
+                className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-full transition cursor-pointer"
+            >
+                {showMore ? 'Hide' : 'Get to know me'}
+            </button>
+
+            {
+                showMore && (
+                    <div className={classNames(
+                        'transition-all duration-500 ease-in-out overflow-hidden',
+                        showMore ? 'max-h-[1000px] mt-6 opacity-100' : 'max-h-0 mt-0 opacity-0'
+                    )}>
+                        <div className="border border-sky -600 rounded-xl p-6 bg-gray-700 text-left text-gray-200 shadow-lg">
+                            {details.map((paragraph, idx) => (
+                                <p className="mb-4">{paragraph}</p>
+                            ))}
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    );
+}
--- src/app/components/About/AboutSection.tsx
+++ src/app/components/About/AboutSection.tsx
@@ -3,9 +3,9 @@
 import Image from "next/image";
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
-import classNames from "classnames";
 import technologies from '@/data/technologies.json';
 import AboutAvatar from "./AboutAvatar";
+import AboutDetails from "./AboutDetails";
 
 export default function About() {
     const [showMore, setShowMore] = useState(false);
@@ -20,30 +20,7 @@ export default function About() {
                     Oleksii Syrov. DevOps.
                 </h2>
 
-                <button
-                    onClick={() => setShowMore(!showMore)}
-                    className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-full transition cursor-pointer"
-                >
-                    {showMore ? 'Hide' : 'Get to know me'}
-                </button>
-
-                {showMore && (
-                    <div
-                        className={classNames(
-                            'transition-all duration-500 ease-in-out overflow-hidden',
-                            showMore ? 'max-h-[1000px] mt-6 opacity-100' : 'max-h-0 mt-0 opacity-0'
-                        )}
-                    >
-                        <div className="border border-sky-600 rounded-xl p-6 bg-gray-700 text-left text-gray-200 shadow-lg">
-                            <p className="mb-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam earum rem enim exercitationem sapiente...
-                            </p>
-                            <p>
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat nesciunt eos fugiat magnam praesentium...
-                            </p>
-                        </div>
-                    </div>
-                )}
+                <AboutDetails />
 
                 <h2 className="text-3xl font-bold mb-6 mt-6">
                     Technology stack
