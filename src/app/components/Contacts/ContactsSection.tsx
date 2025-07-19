d70cd69acdd21eaf0d9e3d91e6c9f4ec1015d421
Lexani42 <33593776+Lexani42@users.noreply.github.com>
2025-07-19 18:11:47 +0300

    Make caorusel in ContactSections

--- src/app/components/Contacts/ContactsSection.tsx
+++ src/app/components/Contacts/ContactsSection.tsx
@@ -1,8 +1,39 @@
 import contacts from '@/data/contacts.json';
 import Image from 'next/image';
 import Footer from '../Footer';
+import React, { useRef } from 'react';
 
 export default function Contact() {
+  const scrollRef = useRef<HTMLDivElement | null>(null)
+
+  let isDown = false;
+  let startX = 0;
+  let scrollLeft = 0;
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    if (!scrollRef.current) return;
+    isDown = true;
+    startX = e.pageX - scrollRef.current.offsetLeft;
+    scrollLeft = scrollRef.current.scrollLeft;
+  
+
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!isDown || !scrollRef.current) return;
+      const x = e.pageX - scrollRef.current.offsetLeft;
+      const walk = (x - startX) * 1.5;
+      scrollRef.current.scrollLeft = scrollLeft - walk;
+    }
+
+    const handleMouseUp = () => {
+      isDown = false;
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp)
+    }
+
+    document.addEventListener('mousemove', handleMouseMove)
+    document.addEventListener('mouseup', handleMouseUp)
+  }
+
   return (
     <section
       id="contact"
@@ -13,22 +44,38 @@ export default function Contact() {
         <p className="text-lg md:text-xl mb-6">
           Feel free to reach out — I’m always open to discussing DevOps, infrastructure challenges, or collaboration opportunities.
         </p>
-        <div className="flex justify-center gap-6 text-xl">
-          {contacts.map((contact) => (
-            <a
-              key={contact.name}
-              href={contact.link}
-              className="w-[100px] h-[100px] bg-gray-800 rounded-xl p-3 flex items-center justify-center hover:bg-sky-200 transition cursor-pointer"
-            >
-              <Image
-                src={contact.img}
-                alt={contact.name}
-                width={64}
-                height={64}
-                className="object-contain"
-              />
-            </a>
-          ))}
+        <div
+          ref={scrollRef}
+          onMouseDown={handleMouseDown}
+          className='cursor-grab overflow-x-auto px-2 py-4'
+          style={{
+            scrollbarWidth: 'none',
+            msOverflowStyle: 'none'
+          }}
+        >
+          <style jsx>{`
+              div::-webkit-scrollbar {
+                display: none;
+              }
+          `}</style>
+
+          <div className="flex snap-x snap-mandatory gap-4">
+            {contacts.map((contact) => (
+              <a
+                key={contact.name}
+                href={contact.link}
+                className='w-[calc(100%/3-1rem)] snap-start flex-shrink-0 h-[100px] bg-gray-800 rounded-xl p-3 flex item-center justify-center hover:bg-sky-200 transition cursor-pointer'
+              >
+                <Image 
+                  src={contact.img}
+                  alt={contact.name}
+                  width={64}
+                  height={64}
+                  className='object-contain'
+                />
+              </a>
+            ))}
+          </div>
         </div>
       </div>
       <div className="pb-10">
