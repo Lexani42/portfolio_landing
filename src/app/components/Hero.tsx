f76dca8105ac3c50f985a9f837211a6ca36c9217
Lexani42 <33593776+Lexani42@users.noreply.github.com>
2025-07-23 01:17:46 +0300

    Change hero component text

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,10 +5,10 @@ export default function Hero() {
         // snap-start snap-always min-h-screen px-6 py-20 text-slate-100
         <section id="hero" className="snap-start snap-always min-h-screen flex flex-col justify-center items-center text-center px-6 text-white">
             <h1 className="text-4xl md:text-6xl font-bold mb-4">
-                🚀 DevOps that ships. Fast.
+                🚀 DevOps. Done right.
             </h1>
             <p className="text-lg md:text-2xl max-w-2xl mb-8">
-                I automate infrastructure, CI/CD and monitoring. You can ship confidently and scale effortlessly.
+                Automation, infrastructure, and observability — built for speed, designed for reliability.
             </p>
             <a href="#contact" className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-full transition">
                 Contact Me
